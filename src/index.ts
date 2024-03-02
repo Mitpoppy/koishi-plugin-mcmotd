f66c930b3f981c116817410c8518522cb6fc7646
“Mitpoppy” <2974560682@qq.com>
2024-03-02 16:36:05 +0800

    测试

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export const name = 'rw-ip'
 export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
-
+1111
 export function apply(ctx: Context) {
   // write your plugin here
 }
